aacf2b016f2ce82501111c293a95feb66f666311
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-25 23:57:28 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,8 +1,8 @@
 /* Macros */
 #define NO_DEVIATION_DETECTED 1
 #define READINGS_DEVIATION_DETECTED 0
-#define SOC 1
-#define CURRENT 2
+#define SOC 0
+#define CURRENT 1
 #define SOC_MAX_THRESHOLD (double)0.05
 #define CURRENT_MAX_THRESHOLD (double)0.1
 #define INVALID_PARAMETER_REQUESTED 0
